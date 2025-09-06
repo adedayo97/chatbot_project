8399bf79474b8eaa34038430207c07d795deb52b
Adedayo97 <adedotunafeez97@gmail.com>
2025-09-06 09:11:21 +0000

    Save local changes to email_utils.py

--- chatbot/email_utils.py
+++ chatbot/email_utils.py
@@ -7,19 +7,19 @@ from django.conf import settings
 def send_verification_email(user_inquiry):
     """Send verification email to user"""
     subject = "Verify Your Email Address - CPI Technologies"
-    
+
     # Create verification link for development
-    verification_link = f"http://127.0.0.1:8000/verify-email/{user_inquiry.verification_token}/"
-    
+    verification_link = f"http://adedayo.pythonanywhere.com/verify-email/{user_inquiry.verification_token}/"
+
     # HTML email content
     html_message = render_to_string('chatbot/verification_email.html', {
         'name': user_inquiry.name,
         'verification_link': verification_link,
         'service': user_inquiry.service,
     })
-    
+
     plain_message = strip_tags(html_message)
-    
+
     try:
         send_mail(
             subject,
